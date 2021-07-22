00699b6539c94e09ac8bae0917e74690784d657c
DarkGhosthunter <DarkGhostHunter@gmail.com>
2021-07-22 01:23:27 -0400

    Fixes migrations publishing.

--- tests/ServiceProviderTest.php
+++ tests/ServiceProviderTest.php
@@ -5,9 +5,33 @@ namespace Tests;
 use DarkGhostHunter\Laraconfig\Facades\Setting;
 use DarkGhostHunter\Laraconfig\LaraconfigServiceProvider;
 use DarkGhostHunter\Laraconfig\Registrar\SettingRegistrar;
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Carbon;
 
 class ServiceProviderTest extends BaseTestCase
 {
+    /** @var \Illuminate\Support\Carbon */
+    static protected $now;
+
+    /** @var \Illuminate\Filesystem\Filesystem */
+    protected mixed $filesystem;
+
+    public static function setUpBeforeClass(): void
+    {
+        parent::setUpBeforeClass();
+
+        static::$now = Carbon::create(2020, 1, 1, 19, 30);
+
+        Carbon::setTestNow(static::$now);
+    }
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->filesystem = $this->app->make(Filesystem::class);
+    }
+
     public function test_registers_package(): void
     {
         static::assertArrayHasKey(LaraconfigServiceProvider::class, $this->app->getLoadedProviders());
@@ -33,8 +57,46 @@ class ServiceProviderTest extends BaseTestCase
             ]
         )->execute();
 
-        $this->assertFileEquals(base_path('config/laraconfig.php'), __DIR__ . '/../config/laraconfig.php');
+        static::assertFileEquals(base_path('config/laraconfig.php'), __DIR__ . '/../config/laraconfig.php');
+    }
 
-        unlink(base_path('config/laraconfig.php'));
+    public function test_publishes_migrations(): void
+    {
+        $this->filesystem->ensureDirectoryExists(database_path('migrations'));
+
+        $this->artisan(
+            'vendor:publish',
+            [
+                '--provider' => 'DarkGhostHunter\Laraconfig\LaraconfigServiceProvider',
+                '--tag' => 'migrations',
+            ]
+        )->run();
+
+        static::assertFileEquals(
+            database_path('migrations/2020_01_01_193000_create_user_settings_table.php'),
+            __DIR__ . '/../database/migrations/00_00_00_000000_create_user_settings_table.php'
+        );
+
+        static::assertFileEquals(
+            database_path('migrations/2020_01_01_193000_create_user_settings_metadata_table.php'),
+            __DIR__ . '/../database/migrations/00_00_00_000000_create_user_settings_metadata_table.php'
+        );
+    }
+
+    protected function tearDown(): void
+    {
+        if ($this->filesystem->exists(base_path('config/laraconfig.php'))) {
+            $this->filesystem->delete(base_path('config/laraconfig.php'));
+        }
+
+        if ($this->filesystem->exists(database_path('00_00_00_000000_create_user_settings_metadata_table.php'))) {
+            $this->filesystem->delete(database_path('00_00_00_000000_create_user_settings_metadata_table.php'));
+        }
+
+        if ($this->filesystem->exists(database_path('00_00_00_000000_create_user_settings_metadata_table.php'))) {
+            $this->filesystem->delete(database_path('00_00_00_000000_create_user_settings_metadata_table.php'));
+        }
+
+        parent::tearDown();
     }
 }
